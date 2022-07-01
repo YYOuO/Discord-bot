db864dbce51b253450b40fae560884112e4ff84e
YYOuO <eddy101173@gmail.com>
2022-07-02 00:14:13 +0800

    json setting

--- bot.py
+++ bot.py
@@ -1,23 +1,27 @@
 import discord
 from discord.ext import commands
-
+# 導入json
+import json
+# 導入json檔
+with open('setting.json', "r", encoding="utf8") as jfile:
+    jdata = json.load(jfile)
 # 建置bot實體 以bot為代詞
 # command_prefix=""前綴詞
 bot = commands.Bot(command_prefix='/')
 
 
-@bot.event
+@bot.event  # event
 async def on_ready():
     # 是否啟動
     print('BOT is online!!')
 # 啟動bot
 
 
-channel = bot.get_channel(992087111144579184)
+channel = bot.get_channel(jdata['channel'])
 
 
-@bot.command()
+@bot.command()  # command
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
-bot.run('OTkxMzYzMDUwNDI4OTYwODQ4.GaS3RB.Hk2vqvw2sqmH3VCbrG2c6dXCF9YR6Xn86pVPcU')
+bot.run(jdata['TOKEN'])
