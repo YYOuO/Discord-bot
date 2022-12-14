f26ab613d0e7430d37029127cbe312cdbaa52bd8
YYOuO <eddy101173@gmail.com>
2022-12-15 00:19:02 +0800

    modify list struct

--- cog/note.py
+++ cog/note.py
@@ -18,27 +18,28 @@ class Notes(cog_extension):
             CREATE TABLE IF NOT EXISTS `NOTE` 
             ( USER INT , 
             MESSAGE TEXT , 
-            TIMEE TIMESTAMP,
-            PRIVATE BOOL);
+            TIMEE TIMESTAMP);
             ''')
         self.sql = False
         self.con.commit()
         self.note_list = []
-        self.time = []
+        self.clear_list = []
+        self.time = 0
         notes = self.cur.execute(
             "SELECT * FROM `NOTE`  ORDER BY `TIMEE` DESC")
         for a in notes.fetchall():
-            self.time.append(int(a[2]))
             self.note_list.append(a)
 
         async def nowtime():
             await self.bot.wait_until_ready()
             while not self.bot.is_closed():
-                time = int(datetime.datetime.today().strftime('%Y%m%d%H%M'))
-                print(time)
-                self.time.sort()
-                if time > self.time[0]:
-                    await self.channel.send(f'Hey! {self.bot.get_user(int(self.note_list[0][0])).mention} {self.note_list[0][1]}')
+                self.time = int(
+                    datetime.datetime.today().strftime('%Y%m%d%H%M'))
+                print(f'Now time: {self.time}')
+                for a in self.note_list:
+                    if self.time > a[2]:
+                        self.clear_list.append(a)
+                        await self.channel.send(f'Hey! {self.bot.get_user(int(self.note_list[a][0])).mention} {a[1]}')
                 await asyncio.sleep(300)
 
         self.timesssss = self.bot.loop.create_task(nowtime())
@@ -52,7 +53,7 @@ class Notes(cog_extension):
         await ctx.send('------------------')
 
     @commands.command()
-    async def create(self, ctx, name, time, private=0):
+    async def create(self, ctx, name, time):
         author = int(ctx.author.id)
         if len(time) != 12:
             await ctx.send('Please follow the style yyyymmddhhmm ex. 202201190857')
@@ -61,14 +62,12 @@ class Notes(cog_extension):
             if int(time):
                 self.sql = True
                 self.cur.execute(
-                    "INSERT INTO `NOTE` (`USER`,`MESSAGE`,`TIMEE`,`PRIVATE`) VALUES(?,?,?,?)", (author, name, time, private))
+                    "INSERT INTO `NOTE` (`USER`,`MESSAGE`,`TIMEE`) VALUES(?,?,?)", (author, name, time))
                 self.con.commit()
-            await ctx.send(content='test', delete_after=5.0)
-            await ctx.send(f'**{name}** create success!')
+            await ctx.send(f'**{name.upper()}** create success!', delete_after=5.0)
             notes = self.cur.execute(
-                "SELECT * FROM `NOTE`  ORDER BY `TIMEE` DESC")
+                "SELECT * FROM `NOTE` ORDER BY `TIMEE` DESC")
             for a in notes.fetchall():
-                self.time.append(int(a[2]))
                 self.note_list.append(a)
             self.sql = False
         else:
@@ -76,27 +75,19 @@ class Notes(cog_extension):
 
     @commands.command()
     async def delete(self, ctx, name):
-        author = int(ctx.author.id)
-        dell = self.cur.execute(
-            "SELECT * FROM NOTE WHERE MESSAGE=(?)", (name,))  # ',' make it a tuple
-        dell = dell.fetchall()
-        print(dell)
-        if(dell[0][0] == '0'):
-            if dell[0] == author:
-                await ctx.send('you are author!')
-            else:
-                await ctx.send('private message!')
-                return 'Falied!'
+        if self.sql == False:
+            self.sql = True
+            self.cur.execute("DELETE FROM NOTE WHERE MESSAGE=(?)", (name,))
+            self.con.commit()
+            self.sql = False
+            for a in self.note_list:
+                if name in a:
+                    self.note_list.remove(a)
+                    if a in self.clear_list:
+                        self.clear_list.remove(a)
+            await ctx.send('Delete success!')
         else:
-            if self.sql == False:
-                self.sql = True
-                self.cur.execute("DELETE FROM NOTE WHERE MESSAGE=(?)", (name,))
-                self.con.commit()
-                self.sql = False
-                self.note_list.remove(name)
-                await ctx.send('Delete success!')
-            else:
-                ctx.send('Wait for the last query finish!')
+            ctx.send('Wait for the last query finish!')
 
 
 async def setup(bot):
