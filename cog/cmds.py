90ad4a072e30823c5bd7cfdf1449fed20479d683
YYOuO <eddy101173@gmail.com>
2023-01-23 23:42:12 +0800

    update cmds:app_command.command

--- cog/cmds.py
+++ cog/cmds.py
@@ -6,6 +6,7 @@ import time
 from requests import get
 import random
 import sys
+from discord import app_commands
 sys.path.append('..')
 with open('setting.json', "r", encoding="utf8") as jfile:
     jdata = json.load(jfile)
@@ -13,57 +14,33 @@ with open('setting.json', "r", encoding="utf8") as jfile:
 
 class Cmds(cog_extension):
 
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.channel.send(f'{round(self.bot.latency*1000)}(ms)')
+    @app_commands.command(name='ping', description='latnecy of bot')
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f'Pong! {round(self.bot.latency*1000)}(ms)')
 
-    @commands.command()
-    async def add(self, ctx):
-        guild = self.bot.get_guild(866673958005506059)
-        role = guild.get_role(993152574289104946)
-        await ctx.author.add_roles(role)
-        await ctx.channel.send(f'{ctx.author.mention} get the permission {role.mention}!')
-        print(f'{ctx.author} get the role')
-        channel = self.bot.get_channel(990988022143123476)
-        await channel.connect()
+    @app_commands.command(name='add', description='add role')
+    async def add(self, interaction: discord.Interaction):
+        role = interaction.user.guild.get_role(993152574289104946)
+        await interaction.user.add_roles(role)
+        await interaction.response.send_message(f'{interaction.user.mention} get the permission {role}!')
+        print(f'{interaction.user} get the role')
 
-    @commands.command()
-    async def picture(self, ctx):
-        random_picture = random.choice(jdata['rickroll'])
-        pic = discord.File(random_picture)
-        await ctx.channel.send(file=pic)
-    start = 0
-
-    @commands.command()
-    async def go(self, ctx):
-        self.start = self.start + time.time()
-        await ctx.channel.send("start count")
-
-    @commands.command()
-    async def stop(self, ctx):
-        self.end = time.time()
-        await ctx.channel.send(f'totaltime:{self.end-self.start}(s)')
-
-    @commands.command()
-    async def draw(self, ctx, quantity, *choose):
-        choose = list(choose)
+    @app_commands.command(name='draw', description='Take a draw for you')
+    async def draw(self, interaction: discord.Interaction, quantity: int, choices: str):
+        choices = choices.split()
         quantity = int(quantity)
-        answer = []
+        answer = ''
         for a in range(quantity):
-            tmp = random.choice(choose)
-            choose.remove(tmp)
-            answer.append(tmp)
-        await ctx.send(f'就決定是你了！出來吧 {answer}！')
+            tmp = random.choice(choices)
+            answer += tmp
+            if a != range(quantity)-1:
+                answer += ' , '
+        await interaction.response.send_message(f'就決定是你了！出來吧 {answer}！')
 
-    @commands.command(pass_context=True)
-    async def poke(self, ctx, member: discord.User, *, message=None):
-        message = message or "lasfsdsflkasjdfdflsaasjdsf"
-        await self.bot.send_message(member, message)
-
-    @commands.command()
-    async def dmme(self, ctx):
-        dm = await self.bot.create_dm(ctx.author)
-        await dm.send('whatever')
+    @app_commands.command(name='dmme', description='create dm')
+    async def dmme(self, interaction: discord.Interaction):
+        dm = interaction.user.create_dm()
+        await dm.send('what')
 
 
 async def setup(bot):
