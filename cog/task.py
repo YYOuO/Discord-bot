dac39efc8793e58aa5b3c2bb0eb32d4a91847c6c
YYOuO <eddy101173@gmail.com>
2023-01-27 00:41:11 +0800

    fix task:add tasks.start function

--- cog/task.py
+++ cog/task.py
@@ -15,21 +15,22 @@ class Task(cog_extension):
         # 父類別初始化屬性(就是搬過來用)
         super().__init__(bot)
         self.channel = self.bot.get_channel(992786432030679070)
-
-        # checknews
         self.old_normal = set()
         self.old_top = set()
+        self.checkrole.start()
+        self.checknews.start()
+        self.TOJcheck.start()
 
-        async def embedit(self, URL, TITLE, DESCRIPTION, GROUP, TOP, CHANNEL):
-            url = 'https://www.tnfsh.tn.edu.tw/latestevent/'+URL
-            embed = discord.Embed(
-                title=TITLE, url=url, description=DESCRIPTION, color=0x6ad771)
-            embed.set_author(
-                name=GROUP, icon_url="https://upload.wikimedia.org/wikipedia/zh/thumb/e/e4/TNFSH_emblem.svg/1200px-TNFSH_emblem.svg.png")
-            embed.set_thumbnail(
-                url="https://www.tnfsh.tn.edu.tw/images/logo.png")
-            embed.set_footer(text=TOP)
-            await CHANNEL.send(embed=embed)
+    async def embedit(self, URL: str, TITLE: str, DESCRIPTION: str, GROUP: str, TOP: str, CHANNEL: str) -> None:
+        url = 'https://www.tnfsh.tn.edu.tw/latestevent/'+URL
+        embed = discord.Embed(
+            title=TITLE, url=url, description=DESCRIPTION, color=0x6ad771)
+        embed.set_author(
+            name=GROUP, icon_url="https://upload.wikimedia.org/wikipedia/zh/thumb/e/e4/TNFSH_emblem.svg/1200px-TNFSH_emblem.svg.png")
+        embed.set_thumbnail(
+            url="https://www.tnfsh.tn.edu.tw/images/logo.png")
+        embed.set_footer(text=TOP)
+        await CHANNEL.send(embed=embed)
 
     @tasks.loop(minutes=20.0)
     async def checkrole(self):
@@ -37,58 +38,53 @@ class Task(cog_extension):
         guild = self.bot.get_guild(int(jdata['guild']))
         voice_channel = self.bot.get_channel(int(jdata['voice']))
         role = guild.get_role(int(jdata['role']))
-        while not self.bot.is_closed():
-            print('role check interval')
-            for member in guild.members:
-                try:
-                    usrvoice = member.voice.channel
-                    if usrvoice != voice_channel:
-                        await member.remove_roles(role)
-                        print(f'{member} role remove')
-                    else:
-                        await voice_channel.connect()
-                except:
+        print('role check interval')
+        for member in guild.members:
+            try:
+                usrvoice = member.voice.channel
+                if usrvoice != voice_channel:
                     await member.remove_roles(role)
-                await asyncio.sleep(5)
-        # chceknews
+                    print(f'{member} role remove')
+                else:
+                    await voice_channel.connect()
+            except:
+                await member.remove_roles(role)
+            await asyncio.sleep(5)
 
+    # chceknews
     tnfsh = "https://www.tnfsh.tn.edu.tw/latestevent/Index.aspx?Parser=9,3,19"
     tnfsh_notify = TnfshNotify(tnfsh)
 
     @tasks.loop(minutes=30.0)
     async def checknews(self, tnfsh_notify=tnfsh_notify):
         await self.bot.wait_until_ready()
-        self.channel = self.bot.get_channel(992786432030679070)
-        while not self.bot.is_closed():
-            print('checknews start')
-            normal_list = set(tnfsh_notify.getNormalList())
-            top_list = set(tnfsh_notify.getPuttopList())
-            nl = normal_list-self.old_normal
-            tl = top_list-self.old_top
-            await asyncio.sleep(30)
-            for a in tl:
-                await self.embedit(a.getUrl(), a.getText(), a.getClaimDate(), a.getClaimGroup(), "置頂！", self.channel)
-            await asyncio.sleep(10)
-            for a in nl:
-                await self.embedit(a.getUrl(), a.getText(), a.getClaimDate(), a.getClaimGroup(), "一般！", self.channel)
-            self.old_normal = normal_list
-            self.old_top = top_list
-            print('Check finish!')
-            await asyncio.sleep(1800)
+        print('checknews start')
+        normal_list = set(tnfsh_notify.getNormalList())
+        top_list = set(tnfsh_notify.getPuttopList())
+        nl = normal_list-self.old_normal
+        tl = top_list-self.old_top
+        await asyncio.sleep(30)
+        for a in tl:
+            await self.embedit(a.getUrl(), a.getText(), a.getClaimDate(), a.getClaimGroup(), "置頂！", self.channel)
+        await asyncio.sleep(10)
+        for a in nl:
+            await self.embedit(a.getUrl(), a.getText(), a.getClaimDate(), a.getClaimGroup(), "一般！", self.channel)
+        self.old_normal = normal_list
+        self.old_top = top_list
+        print('Check finish!')
 
     # TOJ check
     @tasks.loop(minutes=30.0)
     async def TOJcheck(self):
-        while not self.bot.is_closed():
-            print('TOJcheck start')
-            TOJ = get('https://toj.tfcis.org/oj/')
-            self.channel = self.bot.get_channel(int(jdata['channel']))
-            user = ['866673984731742239', '876805556122288138']
-            if TOJ.status_code != 200:
-                for usr in user:
-                    await self.channel.send(self.bot.get_user(int(usr)).mention)
-                await self.channel.send(f'HTTP status CODE :{TOJ.status_code}')
-            await asyncio.sleep(1800)
+        await self.bot.wait_until_ready()
+        print('TOJcheck !')
+        TOJ = get('https://toj.tfcis.org/oj/')
+        self.channel = self.bot.get_channel(int(jdata['channel']))
+        user = ['866673984731742239', '876805556122288138']
+        if TOJ.status_code != 200:
+            for usr in user:
+                await self.channel.send(self.bot.get_user(int(usr)).mention)
+            await self.channel.send(f'HTTP status CODE :{TOJ.status_code}')
 
 
 async def setup(bot):
