0c1122a9bcb827d9effc902f06fd7520c76effc7
YYOuO <eddy101173@gmail.com>
2024-03-31 18:39:42 +0800

    update news check with set-tuple

--- TnfshNotifyApi.py
+++ TnfshNotifyApi.py
@@ -1,18 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-__all__     = ['NotifyElement', 'TnfshNotify']
+__all__ = ["NotifyElement", "TnfshNotify"]
 __version__ = "1.2.0"
-__author__  = "tobiichi3227"
+__author__ = "tobiichi3227"
 # Developed By tobiichi3227
+
+# convert it into tuple for easier manipulation
+
 
 class NotifyElement:
     def __init__(self) -> None:
-        self.__text        = ''
-        self.__url         = ''
-        self.__claim_date  = ''
-        self.__claim_group = ''
-        self.__is_puttop   = False
+        self.__text = ""
+        self.__url = ""
+        self.__claim_date = ""
+        self.__claim_group = ""
+        self.__is_puttop = False
 
     def getText(self) -> str:
         return self.__text
@@ -44,10 +47,11 @@ class NotifyElement:
     def _setPuttop(self, puttop: bool) -> None:
         self.__is_puttop = puttop
 
+
 class TnfshNotify:
     def __init__(self, url: str) -> None:
         self.__url = url
-        self.__html_data = BeautifulSoup(requests.get(self.__url).text, 'html.parser')
+        self.__html_data = BeautifulSoup(requests.get(self.__url).text, "html.parser")
         self.__normal_list = []
         self.__puttop_list = []
         self.__search_limit = 0
@@ -74,26 +78,47 @@ class TnfshNotify:
 
     def __gen_notify_data(self) -> None:
         if self.__search_limit == 0:
-            title_list  = self.__html_data.find_all('span', {'class': 'list_word text_le'})
+            title_list = self.__html_data.find_all(
+                "span", {"class": "list_word text_le"}
+            )
         else:
-            title_list  = self.__html_data.find_all('span', {'class': 'list_word text_le'}, limit=self.__search_limit)
+            title_list = self.__html_data.find_all(
+                "span", {"class": "list_word text_le"}, limit=self.__search_limit
+            )
 
         for title in title_list:
-            if title.select('span', {'class': 'puttop'}).__len__() > 0:
-                element = NotifyElement()
-                element._setUrl(title.select_one('a').get('href'))
-                element._setText(title.select_one('a').getText())
-                element._setClaimGroup(title.find_next_siblings('span', limit=2)[0].getText())
-                element._setClaimDate(title.find_next_siblings('span', limit=2)[1].getText())
-                element._setPuttop(True)
-                self.__puttop_list.append(element)
+            if title.select("span", {"class": "puttop"}).__len__() > 0:
+                # element = NotifyElement()
+                # element._setUrl(title.select_one('a').get('href'))
+                # element._setText(title.select_one('a').getText())
+                # element._setClaimGroup(title.find_next_siblings('span', limit=2)[0].getText())
+                # element._setClaimDate(title.find_next_siblings('span', limit=2)[1].getText())
+                # element._setPuttop(True)
+                # self.__puttop_list.append(element)
+                element = []
+                element.append(title.select_one("a").get("href"))
+                element.append(title.select_one("a").getText())
+                element.append(title.find_next_siblings("span", limit=2)[0].getText())
+                element.append(title.find_next_siblings("span", limit=2)[1].getText())
+                element.append(True)
+                self.__puttop_list.append(tuple(element))
             else:
-                element = NotifyElement()
-                element._setUrl(title.select_one('a').get('href'))
-                element._setText(title.select_one('a').getText())
-                element._setClaimGroup(title.find_next_siblings('span', limit=2)[0].getText())
-                element._setClaimDate(title.find_next_siblings('span', limit=2)[1].getText())
-                self.__normal_list.append(element)
+                # element = NotifyElement()
+                # element._setUrl(title.select_one("a").get("href"))
+                # element._setText(title.select_one("a").getText())
+                # element._setClaimGroup(
+                #     title.find_next_siblings("span", limit=2)[0].getText()
+                # )
+                # element._setClaimDate(
+                #     title.find_next_siblings("span", limit=2)[1].getText()
+                # )
+                # self.__normal_list.append(element)
+                element = []
+                element.append(title.select_one("a").get("href"))
+                element.append(title.select_one("a").getText())
+                element.append(title.find_next_siblings("span", limit=2)[0].getText())
+                element.append(title.find_next_siblings("span", limit=2)[1].getText())
+                self.__normal_list.append(tuple(element))
 
     def UpdateNotifyData(self) -> None:
         self.__normal_list.clear()
--- cog/task.py
+++ cog/task.py
@@ -1,50 +1,68 @@
 import discord
 import json
-with open('./setting.json', 'r', encoding='UTF-8') as jfile:
+
+with open("./setting.json", "r", encoding="UTF-8") as jfile:
     jdata = json.load(jfile)
 from set import cog_extension
 from discord.ext import tasks
 import asyncio
 from TnfshNotifyApi import TnfshNotify
 from requests import get
+from bot import dcBot
 
 
 class Task(cog_extension):
     # 初始化
-    def __init__(self, bot):
-        # 父類別初始化屬性(就是搬過來用)
+    def __init__(self, bot: dcBot):
         super().__init__(bot)
         self.channel = self.bot.get_channel(992786432030679070)
+        self.old_normal = set()
+        self.old_top = set()
         self.old_normal = set()
         self.old_top = set()
         self.checkrole.start()
         self.checknews.start()
         self.TOJcheck.start()
 
-    async def embedit(self, URL: str, TITLE: str, DESCRIPTION: str, GROUP: str, TOP: str, CHANNEL: str) -> None:
-        url = 'https://www.tnfsh.tn.edu.tw/latestevent/'+URL
+    def cog_unload(self):
+        self.checkrole.cancel()
+        self.checknews.cancel()
+        self.TOJcheck.cancel()
+
+    async def embedit(
+        self,
+        URL: str,
+        TITLE: str,
+        DESCRIPTION: str,
+        GROUP: str,
+        TOP: str,
+        CHANNEL: discord.abc.GuildChannel,
+    ):
+        url = "https://www.tnfsh.tn.edu.tw/latestevent/" + URL
         embed = discord.Embed(
-            title=TITLE, url=url, description=DESCRIPTION, color=0x6ad771)
+            title=TITLE, url=url, description=DESCRIPTION, color=0x6AD771
+        )
         embed.set_author(
-            name=GROUP, icon_url="https://upload.wikimedia.org/wikipedia/zh/thumb/e/e4/TNFSH_emblem.svg/1200px-TNFSH_emblem.svg.png")
-        embed.set_thumbnail(
-            url="https://www.tnfsh.tn.edu.tw/images/logo.png")
+            name=GROUP,
+            icon_url="https://upload.wikimedia.org/wikipedia/zh/thumb/e/e4/TNFSH_emblem.svg/1200px-TNFSH_emblem.svg.png",
+        )
+        embed.set_thumbnail(url="https://www.tnfsh.tn.edu.tw/images/logo.png")
         embed.set_footer(text=TOP)
         await CHANNEL.send(embed=embed)
 
     @tasks.loop(minutes=20.0)
     async def checkrole(self):
         await self.bot.wait_until_ready()
-        guild = self.bot.get_guild(int(jdata['guild']))
-        voice_channel = self.bot.get_channel(int(jdata['voice']))
-        role = guild.get_role(int(jdata['role']))
-        print('role check interval')
+        guild = self.bot.get_guild(int(jdata["guild"]))
+        voice_channel = self.bot.get_channel(int(jdata["voice"]))
+        role = guild.get_role(int(jdata["role"]))
+        print("role check interval")
         for member in guild.members:
             try:
                 usrvoice = member.voice.channel
                 if usrvoice != voice_channel:
                     await member.remove_roles(role)
-                    print(f'{member} role remove')
+                    print(f"{member} role remove")
                 else:
                     await voice_channel.connect()
             except:
@@ -58,33 +76,44 @@ class Task(cog_extension):
     async def checknews(self, tnfsh=tnfsh):
         await self.bot.wait_until_ready()
         tnfsh_notify = TnfshNotify(tnfsh)
-        print('checknews start')
+        print("checknews start")
         normal_list = set(tnfsh_notify.getNormalList())
         top_list = set(tnfsh_notify.getPuttopList())
-        nl = normal_list-self.old_normal
-        tl = top_list-self.old_top
-        await asyncio.sleep(30)
-        for a in tl:
-            await self.embedit(a.getUrl(), a.getText(), a.getClaimDate(), a.getClaimGroup(), "置頂！", self.channel)
+        for a in top_list - self.old_top:
+            await self.embedit(
+                a[0],
+                a[1],
+                a[3],
+                a[2],
+                "置頂！",
+                self.channel,
+            )
         await asyncio.sleep(10)
-        for a in nl:
-            await self.embedit(a.getUrl(), a.getText(), a.getClaimDate(), a.getClaimGroup(), "一般！", self.channel)
+        for a in normal_list - self.old_normal:
+            await self.embedit(
+                a[0],
+                a[1],
+                a[3],
+                a[2],
+                "一般！",
+                self.channel,
+            )
         self.old_normal = normal_list
         self.old_top = top_list
-        print('Check finish!')
+        print("Check finish!")
 
     # TOJ check
     @tasks.loop(minutes=30.0)
     async def TOJcheck(self):
         await self.bot.wait_until_ready()
-        print('TOJcheck !')
-        TOJ = get('https://toj.tfcis.org/oj/')
-        self.channel = self.bot.get_channel(int(jdata['channel']))
-        user = ['866673984731742239', '876805556122288138']
+        print("TOJcheck !")
+        TOJ = get("https://toj.tfcis.org/oj/")
+        self.channel = self.bot.get_channel(int(jdata["channel"]))
+        user = ["866673984731742239", "876805556122288138"]
         if TOJ.status_code != 200:
             for usr in user:
                 await self.channel.send(self.bot.get_user(int(usr)).mention)
-            await self.channel.send(f'HTTP status CODE :{TOJ.status_code}')
+            await self.channel.send(f"HTTP status CODE :{TOJ.status_code}")
 
 
 async def setup(bot):
